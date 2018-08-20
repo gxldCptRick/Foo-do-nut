f3dd5e9570ba93377482c425cf8ecd083bcfaa40
ANDRES H CARRERA <andreshcar@live.com>
2018-08-20 09:39:01 -0600

    ignoring all the nesscary files for visual studio and visual studio code

---
+++ Foo(do(nut)) Searcher/DataAccessLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLib
+{
+    public class Class1
+    {
+    }
+}
